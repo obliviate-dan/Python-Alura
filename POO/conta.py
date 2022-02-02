baacabaa08998d7b9e9b637b796a6cc25b541e68
Daniel J. Tenório <daniel.do@hotmail.com>
2022-02-02 17:29:18 -0300

    função transferir

--- POO/conta.py
+++ POO/conta.py
@@ -1,17 +1,26 @@
+'''
+from conta import Conta
+conta2 = Conta(42, "Lupim", 264.34, 500)
+conta = Conta(934, "Fred", 549.54, 2500)
+'''
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def deposito(self, valor):
-        self.saldo += valor
-        print("Saldo atual é de R$ {}".format(self.saldo))
+        self.__saldo += valor
+        print("Saldo atual é de R$ {}".format(self.__saldo))
 
     def saque(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
     def extrato(self):
-        print("Olá, {} seu saldo é de {}".format(self.titular, self.saldo))
+        print("Olá {}, seu saldo é de R${}".format(self.__titular, self.__saldo))
+
+    def transferir(self, valor, destino):
+        self.saque(valor)
+        destino.deposito(valor)
